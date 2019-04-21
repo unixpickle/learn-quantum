b21af34246b9525897e7186a991681ca66019730
Alex Nichol <unixpickle@gmail.com>
2019-04-21 12:35:52 -0400

    bidirectional search
    Memory gods permitting

--- experiments/brute_gate/search.go
+++ experiments/brute_gate/search.go
@@ -1,34 +1,74 @@
 package main
 
 import (
-	"math"
-	"math/cmplx"
+	"crypto/md5"
 	"math/rand"
 
 	"github.com/unixpickle/learn-quantum/quantum"
 )
 
 func Search(numBits, maxGates int, gate func(b []bool) []bool, results chan<- quantum.Circuit) {
-OuterLoop:
-	for {
-		c := RandomCircuit(numBits, rand.Intn(maxGates)+1)
-		for inIdx := 0; inIdx < 1<<uint(numBits); inIdx++ {
-			input := make([]bool, numBits)
-			for i := range input {
-				input[i] = (inIdx&(1<<uint(i)) != 0)
-			}
-			sim := quantum.NewSimulation(numBits)
-			for i, b := range input {
-				if b {
-					quantum.X(sim, i)
-				}
-			}
-			c.Apply(sim)
-			output := gate(input)
-			if math.Abs(cmplx.Abs(sim.Phase(output))-1) > 1e-8 {
-				continue OuterLoop
+	inToOut := make([]int, 1<<uint(numBits))
+	for i := 0; i < 1<<uint(numBits); i++ {
+		input := make([]bool, numBits)
+		for j := range input {
+			input[j] = (i&(1<<uint(j)) != 0)
+		}
+		output := gate(input)
+		outInt := 0
+		for j, b := range output {
+			if b {
+				outInt |= 1 << uint(j)
 			}
 		}
-		results <- c
+		inToOut[i] = outInt
 	}
+
+	forwards := map[string]quantum.Circuit{}
+	backwards := map[string]quantum.Circuit{}
+	for {
+		c := RandomCircuit(numBits, rand.Intn(maxGates/2)+1)
+		var fwdHash string
+		for i := 0; i < (1 << uint(numBits)); i++ {
+			sim := simulationFromBits(numBits, i)
+			c.Apply(sim)
+			fwdHash += "  " + sim.String()
+		}
+		var bwdHash string
+		for _, out := range inToOut {
+			sim := simulationFromBits(numBits, out)
+			c.Invert(sim)
+			bwdHash += "  " + sim.String()
+		}
+
+		fwdHash = hashStr(fwdHash)
+		bwdHash = hashStr(bwdHash)
+
+		if c1, ok := forwards[fwdHash]; !ok || len(c1) > len(c) {
+			forwards[fwdHash] = c
+		}
+		if c1, ok := backwards[bwdHash]; !ok || len(c1) > len(c) {
+			backwards[bwdHash] = c
+		}
+		if c1, ok := backwards[fwdHash]; ok {
+			results <- append(append(quantum.Circuit{}, c...), c1...)
+		} else if c1, ok = forwards[bwdHash]; ok {
+			results <- append(append(quantum.Circuit{}, c1...), c...)
+		}
+	}
+}
+
+func simulationFromBits(numBits, bits int) *quantum.Simulation {
+	res := quantum.NewSimulation(numBits)
+	for i := 0; i < numBits; i++ {
+		if bits&(1<<uint(i)) != 0 {
+			quantum.X(res, i)
+		}
+	}
+	return res
+}
+
+func hashStr(s string) string {
+	sum := md5.Sum([]byte(s))
+	return string(sum[:])
 }
