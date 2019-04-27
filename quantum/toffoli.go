00d5c84685ea9aea9a3787ea70979fbd8973f604
Alex Nichol <unixpickle@gmail.com>
2019-04-27 16:05:00 -0400

    draft N-bit toffoli gate

--- quantum/toffoli.go
+++ quantum/toffoli.go
@@ -1,5 +1,7 @@
 package quantum
 
+import "math"
+
 // CCNot performs a Toffoli gate.
 func CCNot(c Computer, control1, control2, target int) {
 	// https://quantum.country/qcvc
@@ -20,18 +22,51 @@ func CCNot(c Computer, control1, control2, target int) {
 	c.CNot(control1, control2)
 }
 
-// CCCNot performs a four-bit Toffoli gate.
+// ToffoliN performs an n-bit Toffoli gate.
 //
-// This requires that there is some spare qubit on the
-// computer not involved in this gate.
-func CCCNot(c Computer, control1, control2, control3, target int) {
-	working := allocWorking(c, control1, control2, control3, target)
-	if len(working) == 0 {
-		panic("an extra qubit is required")
-	}
-	for i := 0; i < 2; i++ {
-		CCNot(c, control1, control2, working[0])
-		CCNot(c, control3, working[0], target)
+// This typically requires that there is at least one
+// spare qubit on the computer.
+func ToffoliN(c Computer, target int, control ...int) {
+	if len(control) == 0 {
+		X(c, target)
+	} else if len(control) == 1 {
+		c.CNot(control[0], target)
+	} else if len(control) == 2 {
+		CCNot(c, control[0], control[1], target)
+	} else {
+		working := allocWorking(c, target, control...)
+		if len(working) == 0 {
+			panic("not enough working qubits")
+		} else if len(working) >= len(control)-2 {
+			// Lemma 7.2 from Barenco et al. 1995
+			targets := append(append([]int{}, working[:len(control)-2]...), target)
+
+			for i := len(control) - 1; i > 1; i-- {
+				CCNot(c, control[i], targets[i-2], targets[i-1])
+			}
+			CCNot(c, control[0], control[1], targets[0])
+			for i := 2; i < len(control)-1; i++ {
+				CCNot(c, control[i], targets[i-2], targets[i-1])
+			}
+
+			// Undo side-effects
+			for i := len(control) - 2; i > 1; i-- {
+				CCNot(c, control[i], targets[i-2], targets[i-1])
+			}
+			CCNot(c, control[0], control[1], targets[0])
+			for i := 2; i < len(control)-2; i++ {
+				CCNot(c, control[i], targets[i-2], targets[i-1])
+			}
+		} else {
+			// Lemma 7.3 from Barenco et al. 1995
+			size := int(math.Ceil(float64(len(control)+1) / 2))
+			control1 := control[:size]
+			control2 := append([]int{working[0]}, control[size:]...)
+			ToffoliN(c, working[0], control1...)
+			ToffoliN(c, target, control2...)
+			ToffoliN(c, working[0], control1...)
+			ToffoliN(c, target, control2...)
+		}
 	}
 }
 
